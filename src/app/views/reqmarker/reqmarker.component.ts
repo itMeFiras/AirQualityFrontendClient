63ecf0a582b45f27c9f5b32b79989831eaa53362
itMeFiras <47608098+itMeFiras@users.noreply.github.com>
2022-06-16 12:34:33 +0100

    map for the request

--- src/app/views/reqmarker/reqmarker.component.ts
+++ src/app/views/reqmarker/reqmarker.component.ts
@@ -1,17 +1,6 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { LatLngBounds, map, point } from 'leaflet'
-import {
-  IotMapManager,
-  IotMarker,
-  ShapeType,
-  IotMapConfig,
-  TabType,
-  IotMapMarkerManager,
-  IotMapClusterManager,
-  IotMapUserMarkerManager,
-  IotMapPathManager,
-  IotMapAreaManager
-} from 'iotmapmanager'
+import { Component, OnInit } from "@angular/core";
+import * as L from "leaflet";
+//import "leaflet/dist/leaflet.css";
 
 @Component({
   selector: 'app-reqmarker',
@@ -19,97 +8,37 @@ import {
   styleUrls: ['./reqmarker.component.css']
 })
 
-export class ReqmarkerComponent implements AfterViewInit {
-  conf: IotMapConfig = new IotMapConfig();
-  commonIotMap: IotMapManager = new IotMapManager(this.conf)
-  iotMapMarkerManager: IotMapMarkerManager = new IotMapMarkerManager(this.commonIotMap, this.conf)
-  iotMapClusterManager: IotMapClusterManager = new IotMapClusterManager(this.commonIotMap, this.conf)
-  iotMapUserMarkerManager: IotMapUserMarkerManager = new IotMapUserMarkerManager(this.commonIotMap, this.conf)
-  iotMapPathManager: IotMapPathManager = new IotMapPathManager(this.commonIotMap, this.conf)
-  iotMapAreaManager: IotMapAreaManager = new IotMapAreaManager(this.commonIotMap, this.conf)
-  title = 'IotMap';
+export class ReqmarkerComponent implements OnInit {
+  map: L.Map | L.LayerGroup<any> | undefined;
 
-  lists: any[] = [];
-  centerMarkerLat:any
-  centerMarkerLng:any
-  constructor() { }
-
-  ngOnInit(): void {
-    this.conf.setConfig(
-     { map : {defaultLat:36.8349084,defaultLng: 10.2432562} }
-    )
-  }
-
-  markersList: IotMarker[] = []
-
-  ngAfterViewInit (): void {
-    this.conf.setConfig({
-      markerTemplates: {
-        'vehicle': {
-          layer: 'vehicles',
-          shape: {
-            type: ShapeType.circle,
-            anchored: true,
-            plain: true
-          },
-          inner: {
-            icon: 'iotmap-icons-vehicle'
-          }
-        }
-      },
-      map: {
-        externalClustering: false,
-        layerControl: true,
-        exclusiveLayers: false
-      },
-      layerTemplates: {
-        'etablissementssss': {
-          content: '<span class="iotmap-icons-School"></span>',
-          type: TabType.normal,
-          popupColNumber: 2
-        },
-        'meters': {
-          content: '<img width=16 src="../assets/icons/check_your_balance.svg">'
-        },
-        'autos': {
-          content: '<span class="iotmap-icons-4g"></span>',
-          type: TabType.large
-        }
-      },
-      markerStatus: {
-        'test': {
-          name: {
-            singular: 'test',
-            plural: 'tests'
-          },
-          bullet: '<span style="color: #527EDB; font-size: 20px"> &#9678;  </span>',
-          url: 'http://www.orange.com'
-        }
-      }
+  coord:any
+  myMarker:any
+  markerIcon = {
+    icon: L.icon({
+      iconSize: [25, 35],
+      iconAnchor: [10, 35],
+      popupAnchor: [2, -40],
+      // specify the path here
+      iconUrl: /*"https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png"*/"../../../assets/newMa.png",
+      //shadowUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-shadow.png"
     })
+  };
 
-    this.commonIotMap.onMove = () => {
-      const coord = this.commonIotMap.getIotMap().getBounds()
-      this.centerMarkerLat = this.commonIotMap.getIotMap().getCenter().lat
-      this.centerMarkerLng = this.commonIotMap.getIotMap().getCenter().lng
+  ngOnInit() {
+    this.map = L.map("map").setView([36.8349084,  10.2432562], 10);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
 
-      console.log(this.centerMarkerLat,this.centerMarkerLng)
-      console.log('map bounds changed: [' + coord.getNorthEast().lat + ', ' + coord.getNorthEast().lng + '] / [' + coord.getSouthWest().lat + ', ' + coord.getSouthWest().lng + ']')
-    }
-
-
-    this.commonIotMap.onEltClick = (event) =>{
-      const coord = this.commonIotMap.getIotMap().getCenter()
-      console.log(coord.lat)
-      console.log(event)    
-    }
-  
-
-    this.commonIotMap.init('iotMap')
-    this.iotMapMarkerManager.addMarkers(this.markersList)
-    // Do the same when you want to override the zoom on a clicked cluster
-    this.iotMapClusterManager.updateCluster('cluster 1', { markersArea: new LatLngBounds([44.880, 4.89], [44.885, 4.9])})
+    this.map.on("click", e => {
+      if (this.coord !=null){
+        this.map?.removeLayer(this.myMarker);
+      }
+      console.log(e); // get the coordinates
+      this.coord = e
+      this.myMarker = L.marker([this.coord.latlng.lat, this.coord.latlng.lng], this.markerIcon)
+      this.map?.addLayer(this.myMarker);
+    });
   }
-
-
 }
--- src/app/views/request/request.component.ts
+++ src/app/views/request/request.component.ts
@@ -1,25 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , AfterViewInit } from '@angular/core';
 import { UsersService } from 'src/app/models/users.service';
 import { NodesService } from 'src/app/models/nodes.service';
 import { Request } from 'src/app/models/request.model';
-import { Users } from 'src/app/models/users.model';
-
+import * as L from "leaflet";
 
 @Component({
   selector: 'app-request',
   templateUrl: './request.component.html',
   styleUrls: ['./request.component.css']
 })
-export class RequestComponent implements OnInit {
+export class RequestComponent implements AfterViewInit {
 
   constructor(private userService : UsersService, private NodesService:NodesService) { }
   user:any
   request: Request = new Request
   pname:any
 
+//map configurations
+  map: L.Map | L.LayerGroup<any> | undefined;
+  coord:any
+  myMarker:any
+  markerIcon = {
+    icon: L.icon({
+      iconSize: [25, 35],
+      iconAnchor: [10, 35],
+      popupAnchor: [2, -40],
+      // specify the path here
+      iconUrl: /*"https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png"*/"../../../assets/newMa.png",
+      //shadowUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-shadow.png"
+    })
+  };
+
   ngOnInit(): void {
     this.getProfile()
-    console.log(this.user.username)
+    this.locationMap()
+  }
+
+  ngAfterViewInit(){
+    this.locationMap()
   }
 
   getProfile(){
@@ -46,6 +64,27 @@ export class RequestComponent implements OnInit {
       console.log(data)
       window.location.href=`/myNodes`
     })
+  }
+
+  locationMap(){
+    //map configurations
+    this.map = L.map("map").setView([36.8349084,  10.2432562], 10);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    this.map.on("click", e => {
+      if (this.coord !=null){
+        this.map?.removeLayer(this.myMarker);
+      }
+      console.log(e); // get the coordinates
+      this.coord = e
+      this.myMarker = L.marker([this.coord.latlng.lat, this.coord.latlng.lng], this.markerIcon)
+      this.map?.addLayer(this.myMarker);
+      this.request.lat = this.coord.latlng.lat
+      this.request.long = this.coord.latlng.lng
+    });
   }
 
 }
